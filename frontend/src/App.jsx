1ee2f3828b44fd4c153319aec6b5eb788029694a
Ronas Tiwari <120455889+Ronas1zz4@users.noreply.github.com>
2025-02-09 17:06:40 +0545

    router-setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
+import AppRoutes from './routes';
 
 function App() {
   return (
-    <div className="app">
-      <h1 className='text-blue-400'>Welcome to Vite + React</h1>
-      <p>Start building your awesome app!</p>
+    <div>
+      <AppRoutes />
     </div>
   );
 }
